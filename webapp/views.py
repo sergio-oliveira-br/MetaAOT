1c87668daa7c9287169f97511c5cb0ba2ac1d5c6
Sergio <oliveira-sergio@outlook.com>
2026-06-18 18:34:13 +0100

    Update views.py
    act:
    -> extract owner/repo added
    -> Check if is public added

--- webapp/views.py
+++ webapp/views.py
@@ -1,16 +1,54 @@
 # webapp/views.py
 
+import logging
+
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from webapp.forms import UrlForm
+from webapp.services.github.extract_owner_repo import extract_owner_repo_from_url
+from webapp.services.github.check_public import is_github_repository_public
+
+logger = logging.getLogger(__name__)
+
+class CheckPublicError:
+    pass
+
 @require_http_methods(["GET", "POST"])
 def index(request):
     steps_log = []
     result = None
     form = UrlForm(request.POST or None)
+
     if request.method == "POST" and form.is_valid():
         url = form.cleaned_data['url']
-        steps_log.append(f"Initiating analysis for: {url}")
+        steps_log.append(f"1) Initiating analysis for: {url}")
+
+        #extract owner/repo
+        owner_repo = extract_owner_repo_from_url(url)
+        if not owner_repo:
+            steps_log.append("2) URL is not a valid GitHub repository.")
+        else:
+            owner, repo = owner_repo
+            steps_log.append(f"2) Repository detected: {owner}/{repo}")
+
+            # Check if is public
+            try:
+                steps_log.append("3) Checking if the repository is public...")
+                is_public = is_github_repository_public(owner, repo)
+                steps_log.append(f" --> Public Repository: {is_public}")
+                result = {'owner': owner, 'repo': repo, 'repo_public': is_public}
+
+            except CheckPublicError as e:
+                steps_log.append("3) Could not verify the repository on GitHub at the time.")
+                logger.exception("Error when checking if repo is public: %s/%s", owner, repo)
+
+            except Exception:
+                steps_log.append("3) Unexpected error while checking the repository.")
+                logger.exception("Unexpected error while checking repo: %s/%s", owner, repo)
+
+
+        steps_log.append(f"Finished analysis for: {url}")
+
 
     return render(request, 'index.html', {
         'form': form,
